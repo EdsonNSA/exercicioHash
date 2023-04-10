12a4498f2566c130bfc807b79dde3b0f67cb62b5
Edson Nunes <105023369+EdsonNSA@users.noreply.github.com>
2023-04-10 11:28:56 -0300

    Add files via upload

--- hash/EstruturaHashTable.java
+++ hash/EstruturaHashTable.java
@@ -6,25 +6,50 @@ public class EstruturaHashTable implements EstruturaDeDados{
     public EstruturaHashTable() {
         tabela = new Integer[1000];
     }
-
+   
     @Override
     public boolean insert(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
-        return false;
+        int local = chave % 1000;
+        if (tabela[local] == null) 
+        {
+            tabela[local] = chave;
+            return true;
+        }
+        else 
+        {
+            return false;
+        } 
         
+        
+
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
-        return false;
+        int local = chave % 1000;
+        if (tabela[local] != null)
+        {
+            tabela[local] = null;
+            return true;
+        }
+        else 
+        {
+            return false;
+        } 
+        
         
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO se a posição estiver ocupada e valor é o mesmo (ou seja, tem que ser ambos), retorna true. Caso contrário, retorna true.
+        int local = chave % 1000;
+        if(tabela[local] != null)
+        {
+            if(tabela[local] == chave)
+            {
+                return true;
+            }
+        }
         return false;
     }
-    
 }
